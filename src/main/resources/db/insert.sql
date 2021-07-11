3f5873e2bfe89b4911dd29260db4b38dd33b9443
Selesito <selesitomorozov@gmail.com>
2021-07-11 17:37:19 +0400

    Добавление таблиц для пользователя

--- src/main/resources/db/insert.sql
+++ src/main/resources/db/insert.sql
@@ -1,2 +1,9 @@
 insert into posts (name) values ('О чем этот форум?');
 insert into posts (name) values ('Правила форума.');
+
+insert into authorities (authority) values ('ROLE_USER');
+insert into authorities (authority) values ('ROLE_ADMIN');
+
+insert into users (username, enabled, password, authority_id)
+values ('Admin', true, '$2a$10$Vt44.TGBGRn0IBmfWGttVuyBf7.a4FUz.Aby.DggAEo01Mr626tHi',
+        (select id from authorities where authority = 'ROLE_ADMIN'));
