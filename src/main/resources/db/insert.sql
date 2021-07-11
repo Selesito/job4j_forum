8f3a7b53087076804b46bfc21e2ecc4857902ff3
Selesito <selesitomorozov@gmail.com>
2021-07-11 19:24:03 +0400

    Изменение схем и heroku

--- src/main/resources/db/insert.sql
+++ src/main/resources/db/insert.sql
@@ -1,5 +1,9 @@
-insert into posts (name) values ('О чем этот форум?');
-insert into posts (name) values ('Правила форума.');
+insert into posts (name, description) values ('О чем этот форум?', 'Новый форум');
+insert into posts (name, description) values ('Правила форума.', 'Форум' );
 
 insert into authorities (authority) values ('ROLE_USER');
 insert into authorities (authority) values ('ROLE_ADMIN');
+
+insert into users (username, enabled, password, authority_id)
+values ('admin', true, '$2a$10$niVHRW/hgEyEjDkXUlqh3uw2xP1Bc9TM5xxVYNhpLj3aeaSVe8xsq',
+        (select id from authorities where authority = 'ROLE_ADMIN'));
